80ab12fc54a8eccd99de36789938981424a987f0
mgodfre3 <migodfre@microsoft.com>
2019-07-09 09:20:24 -0400

    Create Get-CSVtoPhysicalDisk.ps1

---
+++ Get-CSVtoPhysicalDisk.ps1
@@ -0,0 +1,30 @@
+﻿Function Get-CSVtoPhysicalDiskMapping {
+ 
+    param (
+        [string]$clustername =""
+    )
+ 
+    $clusterSharedVolume = Get-ClusterSharedVolume -Cluster $clusterName
+ 
+    foreach ($volume in $clusterSharedVolume) {
+        $volumeowner = $volume.OwnerNode.Name
+        $csvVolume = $volume.SharedVolumeInfo.Partition.Name
+        $cimSession = New-CimSession -ComputerName $volumeowner
+        $volumeInfo = Get-Disk -CimSession $cimSession | Get-Partition | Select DiskNumber, @{
+                Name="Volume";Expression={Get-Volume -Partition $_ | Select -ExpandProperty ObjectId}
+        }
+ 
+        $csvdisknumber = ($volumeinfo | where { $_.Volume -eq $csvVolume}).Disknumber
+        $csvtophysicaldisk = New-Object -TypeName PSObject -Property @{
+                "CSVName" = $volume.Name
+                "CSVVolumePath" = $volume.SharedVolumeInfo.FriendlyVolumeName
+                "CSVOwnerNode"= $volumeowner
+                "CSVPhysicalDiskNumber"= $csvdisknumber
+                "CSVPartitionNumber"= $volume.SharedVolumeInfo.PartitionNumber
+                "Size (GB)" = [int]($volume.SharedVolumeInfo.Partition.Size/1GB)
+                "FreeSpace (GB)" = [int]($volume.SharedVolumeInfo.Partition.Freespace/1GB)
+        }
+ 
+     $csvtophysicaldisk
+    }
+}
